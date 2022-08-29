2ac20ebcdb7fc7832e646d82cb2c5dad9deea06e
Paulo Ricardo Trintin <pauloricardo@glf.com.br>
2022-08-29 19:01:17 -0300

    testando envs

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Melhor sistema de protese do mundo"
+    return f"""testando envs {os.environ.get("teste")}"""
 
 if __name__ == '__main__':
     app.run()
